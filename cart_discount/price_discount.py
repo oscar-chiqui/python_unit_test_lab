acf955be86228c72f087194ada3deade2a90ca8b
Oscar  A. Chiqui <anibalchiqui88@hotmail.com>
2023-01-31 18:50:50 -0600

    price_discount : DONE !!!!!!!!

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -1,17 +1,33 @@
 def main():
 
-    print(discount([10, 4, 20]))  # Expect this to print 4
-    # what other lists might this function be called with?
+    print(discount([10, 4, 20]))  
+ 
     
 
 def discount(item_prices):
-    """ Complete this function that returns the discount earned for a list of item prices
-    If a customer has ordered more than three items, the cheapest item is free.
-    Example: if this function is called with a list of [10, 4, 20] then return 4.
-    """
 
-    pass  # todo replace this line with your code 
 
+    if type(item_prices) == list:
+        for price in item_prices:
+            if price <0:
+                item_prices.remove(price)
+
+# this function that returns the discount earned for a list of item prices.
+    else:
+        return None
+
+# If a customer has ordered more than three items, the cheapest item is free.
+
+    if len(item_prices) >= 3:
+        item_prices.sort()
+        return item_prices[0]
+
+# if this function is called with a list of [10, 4, 20] then return 4.
+# Return lowest price.
+    else:
+        return None
 
 if __name__ == '__main__':
     main()
+
+# Expect this to print 4
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -9,8 +9,40 @@ class TestDiscount(TestCase):
         expected_discount = 4
         self.assertEqual(expected_discount, discount(prices))
 
-    
-    # TODO more unit tests here. Each test should test one scenario
+# Test scenario - two prices 
+
+    def test_list_of_two_prices(self):
+        prices = [8, 16]
+        expected_discount = None 
+        self.assertEqual(expected_discount, discount(prices))
+
+# Test scenario - one price 
+
+    def test_list_of_one_prices(self):
+        prices =[20]
+        expected_discount = None
+        self.assertEqual(expected_discount, discount(prices))
+
+# test scenario - Zero values 
+
+    def test_prices_that_are_zero(self):
+        prices = [0,0,0,]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+# test scenario - Equal prices - same value 
+
+    def test_equal_prices(self):
+        prices = [9,9,9]
+        expected_discount = 9
+        self.assertEqual(expected_discount, discount(prices))
+
+# test scenario - Negative prices - values 
+
+    def test_negative_numbers(self):
+        prices = [10, 34, -45, -35, -98, -455]
+        with self.assertRaises(ValueError):
+            discount(prices)
 
 
 if __name__ == '__main__':
